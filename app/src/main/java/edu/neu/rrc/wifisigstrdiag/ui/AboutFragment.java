b2a3e2bf25837d13a6f92f745e8d289daaba1bf1
Oleg Vaskevich <oleg.vaskevich@gmail.com>
2014-05-14 16:03:29 -0400

    Restrict number of scans to be greater than 1 Add version to about tab

--- app/src/main/java/edu/neu/rrc/wifisigstrdiag/ui/AboutFragment.java
+++ app/src/main/java/edu/neu/rrc/wifisigstrdiag/ui/AboutFragment.java
@@ -1,19 +1,42 @@
 package edu.neu.rrc.wifisigstrdiag.ui;
 
-import android.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.app.*;
+import android.content.pm.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
 
-import edu.neu.rrc.wifisigstrdiag.R;
+import butterknife.*;
+import edu.neu.rrc.wifisigstrdiag.*;
 
 public class AboutFragment extends Fragment {
+
+    @InjectView(R.id.version) TextView mVersion;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.inject(this, getActivity());
+
+        String version = "???";
+        try {
+            version = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        mVersion.setText(getString(R.string.version, version));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
 
 }
--- app/src/main/java/edu/neu/rrc/wifisigstrdiag/ui/SettingsFragment.java
+++ app/src/main/java/edu/neu/rrc/wifisigstrdiag/ui/SettingsFragment.java
@@ -1,43 +1,28 @@
 
 package edu.neu.rrc.wifisigstrdiag.ui;
 
-import android.content.pm.*;
 import android.os.*;
 import android.preference.*;
-import android.view.*;
-import android.widget.*;
 
-import butterknife.*;
 import edu.neu.rrc.wifisigstrdiag.*;
 
 public class SettingsFragment extends PreferenceFragment {
-
-    @InjectView(R.id.version) TextView mVersion;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.preferences);
+        findPreference(getString(R.string.pref_num_scans_key)).setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object o) {
+                try {
+                    return Integer.parseInt((String) o) > 0;
+                } catch (NumberFormatException nfe) {
+                    return false;
+                }
+            }
+        });
     }
 
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        ButterKnife.inject(this, getActivity());
-
-        String version = "???";
-        try {
-            version = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName;
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        mVersion.setText(getString(R.string.version, version));
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        ButterKnife.reset(this);
-    }
 }
